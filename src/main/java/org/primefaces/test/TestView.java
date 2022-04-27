3c6f7ec066bb5acf0b656cfd1d8cdf9164f40ab6
Marco <none>
2022-04-27 15:05:38 +0200

    Mindmap Reproducer with two Mindmaps only differing in the length of the root labels by 1 char.

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -8,6 +8,8 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import lombok.Data;
+import org.primefaces.model.mindmap.DefaultMindmapNode;
+import org.primefaces.model.mindmap.MindmapNode;
 
 @Data
 @Named
@@ -18,10 +20,28 @@ public class TestView implements Serializable {
     private Integer integer;
     private BigDecimal decimal;
     private LocalDateTime localDateTime;
-    
+
+    private MindmapNode root;
+    private MindmapNode root2;
+
     @PostConstruct  
     public void init() {
         string = "Welcome to PrimeFaces!!!";
+
+        root=new DefaultMindmapNode("GeometrisFormen","Root Data", "#004b76",false);
+        MindmapNode node1=new DefaultMindmapNode("Node No1","Node Date No1", "#99c9e2",false);
+        MindmapNode node2=new DefaultMindmapNode("Node No2","Node Data No2", "#99c9e2",false);
+
+        root.addNode(node1);
+        root.addNode(node2);
+
+
+        root2=new DefaultMindmapNode("GeometriFormen","Root Data", "#004b76",false);
+        MindmapNode node3=new DefaultMindmapNode("Node No1","Node Date No1", "#99c9e2",false);
+        MindmapNode node4=new DefaultMindmapNode("Node No2","Node Data No2", "#99c9e2",false);
+
+        root2.addNode(node3);
+        root2.addNode(node4);
     }
 
 }
